ef2e6ee4e2b078fe70398879c97cbddd0356e3c5
Eda Öney <oneeyyeda@gmail.com>
2025-07-28 21:31:47 +0300

    Completed

---
+++ Liste.cs
@@ -0,0 +1,8 @@
+namespace CartQuantityCalculation;
+
+public class Liste
+{
+    public string Urun { get; set; }
+    public int Fiyat { get; set; }
+
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using CartQuantityCalculation;
+
+var urunler = new List<Liste>();
+
+urunler.Add(new Liste
+{
+    Urun = "Bizim Yağ",
+    Fiyat = 100
+});
+
+urunler.Add(new Liste
+{
+    Urun = "Yoğurt",
+    Fiyat = 50,
+});
+
+urunler.Add(new Liste
+{
+    Urun = "Ekmek",
+    Fiyat = 11,
+});
+
+urunler.Add(new Liste
+{
+    Urun = "Makarna",
+    Fiyat = 15,
+});
+
+urunler.Add(new Liste
+{
+    Urun = "Mayonez",
+    Fiyat = 30,
+});
+
+int toplam = 0;
+foreach (var urun in urunler)
+{
+    
+    Console.WriteLine($"Ürün: {urun.Urun}   fiyat: {urun.Fiyat}TL");
+    toplam += urun.Fiyat ;
+}
+
+Console.WriteLine($"Toplam: {toplam}TL");
